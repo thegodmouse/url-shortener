2763b222efab4ea716f25ede01a62d05a3d6f563
thegodmouse <b97083@gmail.com>
2021-06-09 00:39:08 +0800

    add comments for dto

--- dto/request.go
+++ dto/request.go
@@ -1,5 +1,6 @@
 package dto
 
+// CreateURLRequest defines the request format for creating shorten url.
 type CreateURLRequest struct {
 	URL      string `json:"url"`
 	ExpireAt string `json:"expireAt"`
--- dto/response.go
+++ dto/response.go
@@ -1,5 +1,6 @@
 package dto
 
+// CreateURLResponse defines the response format for creating shorten url.
 type CreateURLResponse struct {
 	ID       string `json:"id"`
 	ShortURL string `json:"shortUrl"`
